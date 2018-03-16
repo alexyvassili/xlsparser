e3f17dda046819ea450a08dc2efd2612867c66e4
Alexey Vasilev <escantor@gmail.com>
2018-03-16 16:23:12 +0300

    Add upload df with batchen in db

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import logging
+import warnings
 import os
 import pandas as pd
 from config import startdir, BLACKLIST, bkf_table
 from xlsparser import GpXlsConfig, GpXlsParser
+from progressbar import printProgressBar
+import MySQLdb
 from db import open_db, close_db
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
 
+warnings.filterwarnings('error', category=MySQLdb.Warning)
 
 class XlsIterator:
     def __init__(self, filenames, start=0):
+        print('Initializing...')
         self.current = start
         self.filenames = filenames
         self.end = len(filenames)
@@ -23,7 +29,8 @@ class XlsIterator:
         if self.current >= self.end:
             raise StopIteration
 
-        logging.info(f"Reading file {self.current+1} of {self.end}")
+        print()
+        print(f"Reading file {self.current+1} of {self.end}")
         parser = GpXlsParser(self.filenames[self.current], self.config)
         df = parser.parse()
         self.current += 1
@@ -44,26 +51,89 @@ def get_filenames(startdir):
     filenames = list(filter(not_in_blacklist, filenames))
     return filenames
 
+
+def split_df(df, batch_size):
+    """Разделяет датафрейм на списки словарей заданного размера"""
+    batches = []
+    batch = []
+    for index, row in df.iterrows():
+        batch.append(row.to_dict())
+        if index % batch_size == 0:
+            batches.append(batch.copy())
+            batch.clear()
+    if batch:
+        batches.append(batch)
+    return batches
+
+
+def get_address_from_mysql_warning(warning: Warning):
+    text = warning.args[1].split("'")
+    field = text[1]
+    row_num = text[2].split()[-1]
+    return int(row_num), field
+
+
+def upload_df_with_batches(SQL, df, batch_size=500):
+    # чтобы cursor.execute() это ел, нужно NaN заменить на None
+    df = df.where(df.notnull(), None)
+    batches = split_df(df, batch_size)
+    length = len(batches)
+    logging.info(f'ALL ITEMS: {sum([len(batch) for batch in batches])} IN {length} BATCHES')
+    logging.info(f'DF SHAPE: {df.shape}')
+    cur, conn = open_db()
+    logging.info(f"Uploading items")
+    warnings = []
+    printProgressBar(0, length, prefix='Uploading dataframe to DB:', suffix='Complete', length=100)
+    for i, batch in enumerate(batches):
+        try:
+            cur.executemany(SQL, batch)
+        except MySQLdb.Warning as e:
+            if 'Data truncated for column' in e.args[1]:
+                row_num, field = get_address_from_mysql_warning(e)
+                value = batch[row_num][field]
+                warnings.append(
+                    {
+                        'row_num': row_num,
+                        'file_row_num': batch[row_num]['bkf_row_num'],
+                        'field': field,
+                        'value': value,
+                        'type': type(value),
+                        'length': len(value) if type(value) == str else None,
+                    }
+                )
+            else:
+                print()
+                print('*' * 100)
+                print('Unexpected MySQL Warning:', e)
+                print('*' * 100)
+                print()
+        conn.commit()
+        printProgressBar(i+1, length, prefix='Uploading dataframe to DB:', suffix='Complete', length=100)
+    conn.commit()
+    close_db(cur, conn)
+    if warnings:
+        logging.warning('DURING UPLOAD SOME (1265, "Data truncated for column N at row M") WARNINGS WAS CATCHED: ')
+        print()
+        print('*' * 100)
+        for w in warnings:
+            print(w)
+        print('*' * 100)
+        print()
+
+
 def upload_df(df: pd.DataFrame):
     fields = ', '.join(df.columns)
     values_fields = [f'%({field})s' for field in df.columns]
     values_fields = ', '.join(values_fields)
     SQL = f"""INSERT INTO {bkf_table} ({fields}) \n
                 VALUES ({values_fields})"""
-    # дальше надо писать VALUES %s %s и т д скорее всего это зависит от типа так что надо смотреть и потом передавать кортежи
-    # вот так: sql = "INSERT INTO TABLE_A(COL_A,COL_B) VALUES(%s, %s)"
-    # a_cursor.execute(sql, (val1, val2))
-    print(SQL)
-    # чтобы cursor.execute() это ел, нужно NaN заменить на None
-    df = df.where(df.notnull(), None)
-    cur, conn = open_db()
-    for index, row in df.iterrows():
-        cur.execute(SQL, row.to_dict())
-    conn.commit()
-    close_db(cur, conn)
+    upload_df_with_batches(SQL, df)
+
 
 if __name__ == "__main__":
     filenames = get_filenames(startdir)
-    walkall = XlsIterator(filenames, 9)
-    df = next(walkall)
-    upload_df(df)
+    # file = [i for i in filenames if 'Казань' in i][0]
+    # index = filenames.index(file)
+    walkall = XlsIterator(filenames, 0)
+    for df in walkall:
+        upload_df(df)
